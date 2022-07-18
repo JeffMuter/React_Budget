dcfdbddcc751c3867f59929f5c0226be90d95804
JeffMuter <muterjeffery@gmail.com>
2022-07-18 15:29:22 -0400

    adjusting dummy data

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import Expenses from "./components/Expenses/Expenses";
 const DUMMY_EXPENSES = [
   {
     id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
+    title: "Rent",
+    amount: 970,
+    date: new Date(2020, 1, 30),
   },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: "e2", title: "Oil Change", amount: 50.99, date: new Date(2021, 2, 12) },
   {
     id: "e3",
     title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
+    amount: 1593.78,
+    date: new Date(2021, 1, 1),
   },
   {
     id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
+    title: "Airline Tickets",
+    amount: 850,
     date: new Date(2021, 5, 12),
   },
 ];
@@ -33,15 +33,6 @@ const App = () => {
     });
   };
 
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
-
-  //console.log(<NewExpense onAddExpense={addExpenseHandler} />);
-  //console.log(<Expenses items={expenses} />);
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
